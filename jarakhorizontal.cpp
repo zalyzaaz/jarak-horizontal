a0420da6a29df145afd066fd339b0b15599af17c
zalyzaaz <141386615+zalyzaaz@users.noreply.github.com>
2023-10-04 07:25:27 +0700

    Add files via upload

---
+++ jarakhorizontal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    double v;
+    double s;
+    double sinA;
+    double jarak;
+
+    cin >> v;
+    cin >> s;
+    sinA = sin(2 * 8 * 8.141 / 180);
+    jarak = v * sinA / 10;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
